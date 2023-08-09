9cf55de81fdb5175085e5a69749e06c43e576b5e
Augustine <augustine.kim@griffithuni.edu.au>
2023-08-10 01:37:05 +1000

    try

--- week3/server.js
+++ week3/server.js
@@ -1,6 +1,34 @@
-var express = requrie('express');
-var app = express();
-var http = requrie('http').Server(app);
-app.use(express.static(__dirmame +'/www'));
+const express = require('express');
+const path = require('path');
+const app = express();
 
-let server
+const users = [
+    { email: "email1@com.au", password: "123" },
+    { email: "email2@com.au", password: "456" },
+    { email: "email3@com.au", password: "789" }
+];
+
+// Homepage route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'www', 'form.html'));
+});
+
+// Account page route
+app.get('/account', (req, res) => {
+    res.sendFile(path.join(__dirname, 'www', 'account.html'));
+});
+
+// Login endpoint
+app.post('/login', express.json(), (req, res) => {
+    const { email, password } = req.body;
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+        res.json({ valid: true });
+    } else {
+        res.json({ valid: false });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server started on http://localhost:3000');
+});
