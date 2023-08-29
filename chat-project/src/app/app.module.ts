32df4dbcf0dc0e29862fdad040488d8beab70d57
Augustine <augustine.kim@griffithuni.edu.au>
2023-08-30 05:20:59 +1000

    add the component

--- chat-project/src/app/app.module.ts
+++ chat-project/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { GroupComponent } from './group/group.component';
+import { ChannelComponent } from './channel/channel.component';
+import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    GroupComponent,
+    ChannelComponent,
+    AdminDashboardComponent
   ],
   imports: [
     BrowserModule,
